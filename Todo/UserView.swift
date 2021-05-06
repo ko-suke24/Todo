fd09a04f7fd1ebaec5fa27b6273faf31623b0a79
ko-suke24 <kosuke0322@gmail.com>
2021-05-06 23:19:10 +0900

    UserViewの作成

---
+++ Todo/UserView.swift
@@ -0,0 +1,45 @@
+//
+//  UserView.swift
+//  Todo
+//
+//  Created by 佐藤航輔 on 2021/05/06.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    
+    let image: Image
+    let userName: String
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("aaa")
+                    .foregroundColor(Color.tTitle)
+                    .font(.footnote)
+                Text("\(userName)")
+                    .foregroundColor(Color.tTitle)
+                    .font(.title)
+            }
+            Spacer()
+            Image("profile")
+                .resizable()
+                .frame(width: 60, height: 60)
+                .clipShape(Circle())
+        }
+        .padding()
+        .background(Color.tBackground)
+    }
+}
+
+struct UserView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            UserView(image: Image("profile"),
+                     userName: "UserName")
+            Circle()
+        }
+        
+    }
+}

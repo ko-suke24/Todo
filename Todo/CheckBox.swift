2586b3cb13c4b7632bf2b361071ccb5ed9d9f810
ko-suke24 <kosuke0322@gmail.com>
2021-05-06 21:32:29 +0900

    CheckBox文字表示

--- Todo/CheckBox.swift
+++ Todo/CheckBox.swift
@@ -7,18 +7,37 @@
 
 import SwiftUI
 
-struct CheckBox: View {
-    @Binding var checked: Bool = true
+struct CheckBox<Label>: View where Label: View {
+    @Binding var checked: Bool
+    
+    private var label: ()-> Label
+    
+    public init(checked: Binding<Bool>,
+                @ViewBuilder label: @escaping () -> Label) {
+        self._checked = checked
+        self.label = label
+    }
+    
     var body: some View {
-        Image(systemName: checked ? "checkmark.circle" : "circle")
-            .onTapGesture {
-                self.checked.toggle()
+        HStack {
+            Image(systemName: checked ? "checkmark.circle" : "circle")
+                .onTapGesture {
+                    self.checked.toggle()
             }
+            label()
+        }
     }
 }
 
 struct CheckBox_Previews: PreviewProvider {
     static var previews: some View {
-        CheckBox()
+        VStack {
+            CheckBox(checked: .constant(true)) {
+                Text("牛乳を買う")
+            }
+            CheckBox(checked: .constant(false)) {
+                Text("牛乳を買う")
+            }
+        }
     }
 }

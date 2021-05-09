b1a97ed7da7754c7eef3f273814f8a9f7853b7f1
ko-suke24 <kosuke0322@gmail.com>
2021-05-10 02:04:33 +0900

    タスク追加画面に時間未設定機能を追加

--- Todo/NewTask.swift
+++ Todo/NewTask.swift
@@ -9,15 +9,21 @@ import SwiftUI
 
 struct NewTask: View {
     @State var task: String = ""
-    @State var time: Date = Date()
+    @State var time: Date? = Date()
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("タスク")) {
                     TextField("タスクを入力", text: $task)
                 }
-                Section(header: Toggle(isOn: .constant(true)) {Text("時間を指定する")}) {
-                    DatePicker(selection: $time, label: { Text("日時") })
+                Section(header: Toggle(isOn: Binding(isNotNil: $time, defaultValue:
+                    Date())) {Text("時間を指定する")}) {
+                    if time != nil {
+                        DatePicker(selection: Binding($time, Date()), label: {
+                                    Text("日時") })
+                    } else {
+                        Text("時間未設定").foregroundColor(.secondary)
+                    }
                 }
                 Button(action: {}) {
                     HStack(alignment: .center) {

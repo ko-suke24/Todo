858acdf74e067702fe8549fb9adbd6dd72c76cd7
ko-suke24 <kosuke0322@gmail.com>
2021-05-10 04:39:04 +0900

    タスク追加保存・キャンセル機能

--- Todo/CategoryView.swift
+++ Todo/CategoryView.swift
@@ -8,39 +8,43 @@
 import SwiftUI
 
 struct CategoryView: View {
-    
     var category: TodoEntity.Category
     @State var numberOfTasks = 0
     @State var showList = false
     @Environment(\.managedObjectContext) var viewContext
-    
+    @State var addNewtask = false
     var body: some View {
-        
         VStack(alignment: .leading) {
             Image(systemName: category.image())
                 .font(.largeTitle)
                 .sheet(isPresented: $showList) {
                     TodoList(category: self.category)
-                        .environment(\.managedObjectContext,self.viewContext)
+                        .environment(\.managedObjectContext, self.viewContext)
                 }
-            
+
             Text(category.toString())
             Text("・\(numberOfTasks)タスク")
-            Button(action: {}) {
+            Button(action: {
+                self.addNewtask = true
+            }) {
                 Image(systemName: "plus")
+            }.sheet(isPresented: $addNewtask) {
+                NewTask(category: self.category.rawValue)
+                    .environment(\.managedObjectContext, self.viewContext)
             }
             Spacer()
         }
-        .padding()
+            .padding()
         .frame(maxWidth:.infinity, minHeight: 150)
-        .foregroundColor(.white)
-        .background(category.color())
-        .cornerRadius(20)
+            .foregroundColor(.white)
+            .background(category.color())
+            .cornerRadius(20)
         .onTapGesture {
             self.showList = true
         }
     }
 }
+
 
 struct CategoryView_Previews: PreviewProvider {
     static let context = (UIApplication.shared.delegate as! AppDelegate)
@@ -53,6 +57,6 @@ struct CategoryView_Previews: PreviewProvider {
             CategoryView(category: .ImpNUrg_2nd)
             CategoryView(category: .NImpUrg_3rd)
             CategoryView(category: .NImpNUrg_4th)
-        }
+        }.environment(\.managedObjectContext, context)
     }
 }
--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -12,18 +12,16 @@ struct ContentView: View {
         VStack(spacing: 0) {
             Color.tBackground
                 .edgesIgnoringSafeArea(.top)
-                .frame(height: 0)
-            UserView(image: Image("profile"), userName: "aaaaa")
-            
+                .frame(height:0)
+            UserView(image: Image("profile"), userName: "aaaa")
             VStack(spacing: 0) {
+                
                 HStack(spacing: 0) {
                     CategoryView(category: .ImpUrg_1st)
                     Spacer()
                     CategoryView(category: .ImpNUrg_2nd)
                 }
-                
                 Spacer()
-                
                 HStack(spacing: 0) {
                     CategoryView(category: .NImpUrg_3rd)
                     Spacer()
@@ -31,12 +29,16 @@ struct ContentView: View {
                 }
             }.padding()
         }.background(Color.tBackground)
-        .edgesIgnoringSafeArea(.bottom)
+            .edgesIgnoringSafeArea(.bottom)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static let context = (UIApplication.shared.delegate as! AppDelegate)
+        .persistentContainer.viewContext
+
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, context)
     }
 }

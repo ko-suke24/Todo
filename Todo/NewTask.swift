eb6b287b624df2da65b90bdaab552a4d8762ff65
ko-suke24 <kosuke0322@gmail.com>
2021-05-10 02:25:58 +0900

    タスク追加画面に種類を追加

--- Todo/NewTask.swift
+++ Todo/NewTask.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct NewTask: View {
     @State var task: String = ""
     @State var time: Date? = Date()
+    @State var category: Int16 = TodoEntity.Category.ImpUrg_1st.rawValue
+    var categories: [TodoEntity.Category] = [.ImpUrg_1st, .ImpNUrg_2nd, .NImpUrg_3rd, .NImpNUrg_4th]
     var body: some View {
         NavigationView {
             Form {
@@ -25,11 +27,23 @@ struct NewTask: View {
                         Text("時間未設定").foregroundColor(.secondary)
                     }
                 }
-                Button(action: {}) {
-                    HStack(alignment: .center) {
-                        Image(systemName: "minus.circle.fill")
-                        Text("キャンセル")
-                    }.foregroundColor(.red)
+                
+                Picker(selection: $category, label: Text("種類"), content: {
+                    ForEach(categories, id: \.self) { category in
+                        HStack {
+                            CategoryImage(category)
+                            Text(category.toString())
+                        }.tag(category.rawValue)
+                    }
+                })
+                
+                Section(header: Text("操作")) {
+                    Button(action: {}) {
+                        HStack(alignment: .center) {
+                            Image(systemName: "minus.circle.fill")
+                            Text("キャンセル")
+                        }.foregroundColor(.red)
+                    }
                 }
             }.navigationTitle("タスクの追加")
         }
